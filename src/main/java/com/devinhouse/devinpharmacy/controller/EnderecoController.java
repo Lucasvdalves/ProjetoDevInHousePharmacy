73e7225d6aea0d72f61f813c38ce22f8f4e08df8
Lucas Damasceno <lucasalvesbh2018@gmail.com>
2024-02-11 16:39:33 -0300

    Atualizacao

--- src/main/java/com/devinhouse/devinpharmacy/controller/EnderecoController.java
+++ src/main/java/com/devinhouse/devinpharmacy/controller/EnderecoController.java
@@ -1,12 +1,7 @@
 package com.devinhouse.devinpharmacy.controller;
 
-import com.devinhouse.devinpharmacy.model.Endereco;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/enderecos")
